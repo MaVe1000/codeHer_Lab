384b4688a00362c820927343a2cb3c5073c42e3e
luciadmaddalena <dellam.lucia@outlook.com>
2025-08-13 18:13:54 -0300

    Clase 4

---
+++ Clases/04_Hook_useState_Eventos_/hook-clase/src/Formulario.jsx
@@ -0,0 +1,44 @@
+/* import { useState } from "react";
+
+export const Formulario = () => {
+    //Estado
+    const [valorInput, setValorInput] = useState('');
+    
+    //funcion manejadora del evento onChange
+    const onChange = () => {
+      console.log('Se esta tecleando');
+    }
+
+    //JSX
+    return (
+        <div>
+            <input onChange={onChange} /> 
+            <p>{valorInput}</p>
+        </div>
+    )
+
+}
+ */
+
+
+
+/* import { useState } from "react";
+
+export const Formulario = () => {
+    //Estado
+    const [valorInput, setValorInput] = useState('');
+    
+    //funcion manejadora del evento onChange
+    const onChange = (evento) => {
+      console.log(evento);
+    }
+
+    //JSX
+    return (
+        <div>
+            <input onChange={evento=>onChange(evento)} /> 
+            <p>{valorInput}</p>
+        </div>
+    )
+
+} */
